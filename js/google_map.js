26e70c4c45523ad7d76d9d3734df68575e665b1f
wang <yangwang20102010@gmail.com>
2018-05-18 02:34:21 -0400

    addMap

--- js/google_map.js
+++ js/google_map.js
@@ -1,35 +1,27 @@
+var locations = [
+  ['Stadtbibliothek Zanklhof', 47.06976, 15.43154, 1]
+];
 
-  var locations = [
-    ['Stadtbibliothek Zanklhof', 47.06976, 15.43154, 1],
-    ['Stadtbibliothek dieMediathek', 47.06975, 15.43116, 2],
-    ['Stadtbibliothek Gösting', 47.09399, 15.40548, 3],
-    ['Stadtbibliothek Graz West', 47.06993, 15.40727, 4],
-    ['Stadtbibliothek Graz Ost', 47.06934, 15.45888, 5],
-    ['Stadtbibliothek Graz Süd', 47.04572, 15.43234, 6],
-    ['Stadtbibliothek Graz Nord', 47.08350, 15.43212, 7],
-    ['Stadtbibliothek Andritz', 47.10280, 15.42137, 8]
-  ];
+var map = new google.maps.Map(document.getElementById('map'), {
+  zoom: 13,
+  center: new google.maps.LatLng(45.423602,-75.7008956),
+  mapTypeId: google.maps.MapTypeId.ROADMAP
+});
 
-  var map = new google.maps.Map(document.getElementById('map'), {
-    zoom: 13,
-    center: new google.maps.LatLng(47.071876, 15.441456),
-    mapTypeId: google.maps.MapTypeId.ROADMAP
+var infowindow = new google.maps.InfoWindow();
+
+var marker, i;
+
+for (i = 0; i < locations.length; i++) {
+  marker = new google.maps.Marker({
+    position: new google.maps.LatLng(locations[i][1], locations[i][2]),
+    map: map
   });
 
-  var infowindow = new google.maps.InfoWindow();
-
-  var marker, i;
-
-  for (i = 0; i < locations.length; i++) {
-    marker = new google.maps.Marker({
-      position: new google.maps.LatLng(locations[i][1], locations[i][2]),
-      map: map
-    });
-
-    google.maps.event.addListener(marker, 'click', (function(marker, i) {
-      return function() {
-        infowindow.setContent(locations[i][0]);
-        infowindow.open(map, marker);
-      }
-    })(marker, i));
-  }
+  google.maps.event.addListener(marker, 'click', (function(marker, i) {
+    return function() {
+      infowindow.setContent(locations[i][0]);
+      infowindow.open(map, marker);
+    }
+  })(marker, i));
+}
